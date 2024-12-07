504cea7b8c974c32779ff3d8c3aa9ecb2bf57b7c
gustavclausen <gustav@gustavclausen.com>
2024-12-07 20:45:10 +0100

    day 3 (part 2)

--- day3/main.go
+++ day3/main.go
@@ -26,22 +26,69 @@ func readInput() (string, error) {
 	return string(file), nil
 }
 
-func sumMulInstructs(program string) int {
-	pattern := `mul\((\d+),(\d+)\)`
-
-	reg := regexp.MustCompile(pattern)
-	matches := reg.FindAllStringSubmatch(program, -1)
-
+func calMulInstructs(mulInstructs [][]string) int {
 	sum := 0
 
-	for _, match := range matches {
-		first, _ := strconv.Atoi(match[1])
-		second, _ := strconv.Atoi(match[2])
+	for _, instruct := range mulInstructs {
+		first, _ := strconv.Atoi(instruct[1])
+		second, _ := strconv.Atoi(instruct[2])
 
 		sum += first * second
 	}
 
 	return sum
+}
+
+func findAllMatches(program string, pattern string) [][]string {
+	reg := regexp.MustCompile(pattern)
+
+	return reg.FindAllStringSubmatch(program, -1)
+}
+
+func findFirstMatchIndex(program string, pattern string) []int {
+	reg := regexp.MustCompile(pattern)
+
+	return reg.FindStringIndex(program)
+
+}
+
+func task1(program string) int {
+	mulInstructs := findAllMatches(program, `mul\((\d+),(\d+)\)`)
+
+	return calMulInstructs(mulInstructs)
+}
+
+func task2(program string) int {
+	enable := true
+
+	mulInstructs := [][]string{}
+
+	for {
+		if enable {
+			disableMatch := findFirstMatchIndex(program, `don't\(\)`)
+			subprogram := program
+
+			if len(disableMatch) != 0 {
+				subprogram = program[:disableMatch[0]]
+				program = program[disableMatch[1]:]
+
+				enable = false
+			}
+
+			mulInstructs = append(mulInstructs, findAllMatches(subprogram, `mul\((\d+),(\d+)\)`)...)
+
+			if len(disableMatch) == 0 {
+				break
+			}
+		} else {
+			enableMatch := findFirstMatchIndex(program, `do\(\)`)
+			program = program[enableMatch[1]:]
+
+			enable = true
+		}
+	}
+
+	return calMulInstructs(mulInstructs)
 }
 
 func main() {
@@ -50,5 +97,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Task 1 result: %d\n", sumMulInstructs(program))
+	fmt.Printf("Task 1 result: %d\n", task1(program))
+	fmt.Printf("Task 2 result: %d\n", task2(program))
 }
